0f2f355a1a7502334a54a527fa3bb1fe11f8d367
joaobuso <joao.buso@gmail.com>
2025-05-18 18:35:55 -0300

    Upload Code

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,11 @@ def create_app():
     app = Flask(__name__, static_folder="static_bot_audio")
     
     # Registrar blueprints
-    from .agent.routes import agent_bp
+    from app.agent.routes import agent_bp
     app.register_blueprint(agent_bp)
     
-    # Importar e configurar o webhook principal
-    from . import main
+    # Importar rotas principais
+    from app.main import configure_routes
+    configure_routes(app)
     
     return app

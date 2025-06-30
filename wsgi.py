26bf413f7e8e1a84df52d1378982840c5368b1d3
joaobuso <joao.buso@gmail.com>
2025-06-30 17:31:34 -0300

    Upload Code

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,9 @@
-from flask import Flask
-from app.main import configure_routes
-
-def create_app():
-    app = Flask(__name__, static_folder="app/static_bot_audio")
-    configure_routes(app)
-    return app
+from app import create_app
+from app.agent.routes import agent_bp
 
 app = create_app()
 
+app.register_blueprint(agent_bp)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")

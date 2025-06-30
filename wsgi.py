d98c7c7fd6665096d9ea124504abc60da96ab41e
joaobuso <joao.buso@gmail.com>
2025-06-30 17:38:11 -0300

    Upload Code

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,6 @@
 from app import create_app
-from app.agent.routes import agent_bp
 
 app = create_app()
-
-app.register_blueprint(agent_bp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
